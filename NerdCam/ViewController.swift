8ba4d7b626436b0ef048c98d123de2b78f2d5c18
Tyler Thompson <tyler@tylerpaulthompson.com>
2015-12-03 13:40:24 -0500

    Initial commit

---
+++ NerdCam/ViewController.swift
@@ -0,0 +1,130 @@
+//
+//  ViewController.swift
+//  NerdCam
+//
+//  Created by Jeremy on 2014-09-23.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+import AVFoundation
+import UIKit
+
+extension ViewController: AVCaptureMetadataOutputObjectsDelegate {}
+
+class ViewController: UIViewController {
+
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
+    var hatImage = UIImage(named: "hat.png")!.CGImage
+    var hatLayer: CALayer = {
+        let hat = CALayer()
+        hat.contents = UIImage(named: "kennyg.png")!.CGImage
+        hat.contentsGravity = kCAGravityResizeAspect
+//        hat.transform = CATransform3DMakeScale(2.5, 2.5, 1);
+        return hat
+    }()
+
+    func newVideoCaptureSession() -> AVCaptureSession? {
+        /* Set up a capture input for the default video camera */
+        let videoCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+
+        let videoInput: AVCaptureDeviceInput?
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCamera)
+        } catch let error as NSError {
+            print("\(__FUNCTION__): failed to init capture device" +
+                "with video camera \(videoCamera): error \(error)")
+            videoInput = nil
+        }
+
+        /* Attach the input to a capture session */
+        let captureSession = AVCaptureSession()
+        if !captureSession.canAddInput(videoInput) {
+            print("\(__FUNCTION__): cannot add input \(videoCamera)")
+            return nil
+        }
+
+        captureSession.addInput(videoInput)
+        return captureSession
+    }
+
+    func addPreviewLayerForSession(session: AVCaptureSession) {
+        let layer = AVCaptureVideoPreviewLayer(session: session)
+        let rootLayer = self.view.layer
+        layer.frame = rootLayer.bounds
+        rootLayer.addSublayer(layer)
+        self.view.layer.addSublayer(self.hatLayer)
+
+        previewLayer = layer
+    }
+
+    func addMetadataOutputToSession(session: AVCaptureSession) {
+        let metadataOutput = AVCaptureMetadataOutput()
+        if !session.canAddOutput(metadataOutput) {
+            print("\(__FUNCTION__): cannot add output \(metadataOutput)")
+            return
+        }
+
+        session.addOutput(metadataOutput)
+        let queue = dispatch_queue_create(
+            "com.bignerdranch.advios.NerdCam.MetadataOutput", DISPATCH_QUEUE_SERIAL
+        )
+        metadataOutput.setMetadataObjectsDelegate(self, queue: queue)
+        metadataOutput.metadataObjectTypes = [AVMetadataObjectTypeFace]
+    }
+
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        guard let face = metadataObjects.first as? AVMetadataObject else {
+            hatLayer.hidden = true
+            return
+        }
+
+        let faceRect = self.previewLayer
+            .rectForMetadataOutputRectOfInterest(face.bounds)
+        let scaledFaceRect = CGRectInset(faceRect, -30, -30)
+
+        dispatch_async(dispatch_get_main_queue()) {
+            self.hatLayer.hidden = false
+            self.hatLayer.frame = scaledFaceRect
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        /* Crash if we can't get a capture session */
+        captureSession = newVideoCaptureSession()
+        addPreviewLayerForSession(captureSession)
+        addMetadataOutputToSession(captureSession)
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        captureSession.startRunning()
+    }
+
+    override func viewDidDisappear(animated: Bool) {
+        captureSession.stopRunning()
+        super.viewWillDisappear(animated)
+    }
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+
+    override func viewWillTransitionToSize(
+        size: CGSize,
+        withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator)
+    {
+        previewLayer.frame = CGRect(origin: CGPointZero, size: size)
+        let currentOrientation = UIDevice.currentDevice().orientation
+        let videoOrientation = AVCaptureVideoOrientation(rawValue: currentOrientation.rawValue)!
+        previewLayer.connection.videoOrientation = videoOrientation
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
